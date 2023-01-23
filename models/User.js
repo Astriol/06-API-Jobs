8df9180c4431b8bba9645fef4d36db3ec408c45b
Paige Mundy <paigemundy@Paiges-MacBook-Pro.local>
2023-01-23 14:21:53 -0600

    2nd Update

--- models/User.js
+++ models/User.js
@@ -1,33 +1,54 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
-  name:{
+  name: {
     type: String,
     required: [true, 'Please provide name'],
-    minlength: 3,
     maxlength: 50,
+    minlength: 3,
   },
-  email:{
+  email: {
     type: String,
     required: [true, 'Please provide email'],
-    match:[
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    'please provide valid email',
-  ],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
     unique: true,
   },
-  password:{
+  password: {
     type: String,
     required: [true, 'Please provide password'],
     minlength: 6,
   },
 })
 
-UserSchema.pre('save', async function(){
-  const salt = await bcrypt.genSalt(10);
+// UserSchema.pre('save', async function(){
+//   const saltRounds = 10;
+//   const password = UserSchema.password;
+//   bcrypt.genSalt(saltRounds, function(err, salt) {
+//     bcrypt.hash(password, salt, function(err, hash) {
+//         // Store hash in your password DB.
+//     });
+//   });
+// })
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
-})
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({userId:this._id, name:this.name}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  })
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
+}
 
 module.exports = mongoose.model('User', UserSchema)
